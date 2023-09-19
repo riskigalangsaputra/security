2b2d0af79d19e138652366b6e488c166919b2c14
riskigalangsaputra <riskigalangsaputra@gmail.com>
2023-09-19 12:56:54 +0700

    init

---
+++ src/main/java/com/hades/security/entity/user/Role.java
@@ -0,0 +1,12 @@
+package com.hades.security.entity.user;
+
+/*
+ * @Author  : galang
+ * @Created : 19-09-2023
+ * @Email   : riski.saputra@iconpln.co.id
+ */
+
+public enum Role {
+    USER,
+    ADMIN
+}
